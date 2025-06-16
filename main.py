df40a1d1110a519e3982d6676cf80e3351f5dfa2
shmigovnyak <160512113+shmigovnyak@users.noreply.github.com>
2025-06-16 16:05:30 +0300

    Добавила автоматическое создание таблицы users.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from routes.users_route import router as users_router
 from routes.books_route import router as books_router
+from service.db import engine, Base
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def on_startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 # Включение роутера пользователей
 app.include_router(users_router)
